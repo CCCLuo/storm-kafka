33b48a583114be146bfceac0972d9698ce581ca2
Leezer <li.zenan@tianyuantechnology.com>
2017-12-30 16:48:01 +0800

    Initial commit

---
+++ src/test/java/lzn/storm/stormkafka/StormKafkaLauncherTests.java
@@ -0,0 +1,16 @@
+package lzn.storm.stormkafka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StormKafkaLauncherTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
